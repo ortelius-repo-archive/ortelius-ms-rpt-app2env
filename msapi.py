2024639333671b79129aa1293fcd72cc29b53cfe
Sacha Wharton <sacha@gmail.com>
2020-10-05 19:59:49 +0200

    added waitress server config

--- msapi.py
+++ msapi.py
@@ -75,4 +75,7 @@ api.add_resource(Alien, '/')
 api.add_resource(Places, '/places')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    if (os.getenv('FLASK_DEV_ENV', None) is not None):
+        app.run(host="0.0.0.0", port=5000)
+    else:
+        serve(app, host='0.0.0.0', port=5000)
